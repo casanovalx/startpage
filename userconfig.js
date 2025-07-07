505585144393375be6cc462be71d3b8309ddea46
APOLLO Community Main Dev (casanovaltd) <123438051+casanovalx@users.noreply.github.com>
2025-07-07 19:59:44 +0300

    Add files via upload

---
+++ userconfig.js
@@ -0,0 +1,312 @@
+let saved_config = JSON.parse(localStorage.getItem("CONFIG"));
+
+const default_config = {
+  overrideStorage: true,
+  temperature: {
+    location: 'Minsk',
+    scale: "C",
+  },
+  clock: {
+    format: "h:i",
+    iconColor: "#ea6962",
+  },
+  search: {
+    engines: {
+      g: ["https://google.com/search?q=", "Google"],
+      d: ["https://duckduckgo.com/html?q=", "DuckDuckGo"],
+      y: ["https://youtube.com/results?search_query=", "Youtube"],
+      r: ["https://www.reddit.com/search/?q=", "Reddit"],
+      p: ["https://www.pinterest.com/search/pins/?q=", "Pinterest"],
+    },
+  },
+  keybindings: {
+    "s": "search-bar",
+    "q": "config-tab",
+  },
+  disabled: [],
+  localIcons: true,
+  fastlink: "https://duck.ai/",
+  openLastVisitedTab: true,
+  tabs: [
+    {
+      name: "chi ll",
+      background_url: "src/img/banners/cbg-4.gif",
+      categories: [{
+        name: "Social Media",
+        links: [
+          {
+            name: "mastodon",
+            url: "https://mastodon.social",
+            icon: "brand-mastodon",
+            icon_color: "#6F00FF",
+          },
+          {
+            name: "vk",
+            url: "https://vk.com/feed",
+            icon: "brand-vk",
+            icon_color: "#7daea3",
+          },
+          {
+            name: "reddit",
+            url: "https://www.reddit.com/",
+            icon: "brand-reddit",
+            icon_color: "#e78a4e",
+          },
+          {
+            name: "youtube",
+            url: "https://www.youtube.com/",
+            icon: "brand-youtube-filled",
+            icon_color: "#ea6962",
+          },
+          {
+            name: "twitch",
+            url: "https://www.twitch.tv/",
+            icon: "brand-twitch",
+            icon_color: "#d3869b",
+          },
+        ],
+      }, {
+        name: "Video",
+        links: [
+          {
+            name: "odysee",
+            url: "https://odysee.com/",
+            icon: "mood-search",
+            icon_color: "#FF6600",
+          },
+          {
+            name: "primevideo",
+            url: "https://www.primevideo.com/region/eu/?ref_=dv_web_unknown",
+            icon: "brand-amazon",
+            icon_color: "#7daea3",
+          },
+        ],
+      }],
+    },
+    {
+      name: "design",
+      background_url: "src/img/banners/cbg-4.gif",
+      categories: [
+        {
+          name: "inspiration",
+          links: [
+            {
+              name: "pinterest",
+              url: "https://www.pinterest.es/",
+              icon: "brand-pinterest",
+              icon_color: "#ea6962",
+            },
+            {
+              name: "artstation",
+              url: "https://www.artstation.com/?sort_by=community",
+              icon: "chart-area",
+              icon_color: "#7daea3",
+            },
+            {
+              name: "leonardo ai",
+              url: "https://app.leonardo.ai/",
+              icon: "brand-openai",
+              icon_color: "#89b482",
+            },
+            {
+              name: "dribble",
+              url: "https://dribbble.com/following",
+              icon: "brand-dribbble-filled",
+              icon_color: "#d3869b",
+            },
+          ],
+        },
+        {
+          name: "resources",
+          links: [
+            {
+              name: "figma",
+              url: "https://www.figma.com",
+              icon: "brand-figma",
+              icon_color: "#d3869b",
+            },
+            {
+              name: "uxpro",
+              url: "https://uxpro.cc/",
+              icon: "components",
+              icon_color: "#a9b665",
+            },
+            {
+              name: "colorhunt",
+              url: "https://colorhunt.co/",
+              icon: "color-picker",
+              icon_color: "#ea6962",
+            },
+            {
+              name: "adobe color",
+              url: "https://color.adobe.com/ru/create/color-wheel",
+              icon: "brand-adobe",
+              icon_color: "#7daea3",
+            },
+            {
+              name: "terminalsexy",
+              url: "https://terminal.sexy",
+              icon: "prompt",
+              icon_color: "#e78a4e",
+            },
+          ],
+        },
+        {
+          name: "resources 3d",
+          links: [
+            {
+              name: "thingiverse",
+              url: "https://www.thingiverse.com/",
+              icon: "circle-letter-t",
+              icon_color: "#7daea3",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "dev",
+      background_url: "src/img/banners/cbg-7.gif",
+      categories: [
+        {
+          name: "repositories",
+          links: [
+            {
+              name: "github",
+              url: "https://github.com/",
+              icon: "brand-github",
+              icon_color: "#7daea3",
+            },
+            {
+              name: "gitea",
+              url: "https://gitea.com/",
+              icon: "mug",
+              icon_color: "#00FF91",
+            },
+          ],
+        },
+        {
+          name: "resources",
+          links: [
+            {
+              name: "phind",
+              url: "https://www.phind.com/",
+              icon: "brand-openai",
+              icon_color: "#89b482",
+            },
+            {
+              name: "flutter",
+              url: "https://docs.flutter.dev/ui",
+              icon: "brand-flutter",
+              icon_color: "#7daea3",
+            },
+            {
+              name: "hacktricks",
+              url: "https://book.hacktricks.xyz/welcome/readme",
+              icon: "biohazard",
+              icon_color: "#ea6962",
+            },
+            {
+              name: "vscode",
+              url: "https://vscode.dev/",
+              icon: "brand-vscode",
+              icon_color: "#7daea3",
+            },
+          ],
+        },
+        {
+          name: "challenges",
+          links: [
+            {
+              name: "hackthebox",
+              url: "https://app.hackthebox.com",
+              icon: "box",
+              icon_color: "#a9b665",
+            },
+            {
+              name: "cryptohack",
+              url: "https://cryptohack.org/challenges/",
+              icon: "brain",
+              icon_color: "#e78a4e",
+            },
+            {
+              name: "tryhackme",
+              url: "https://tryhackme.com/dashboard",
+              icon: "brand-onedrive",
+              icon_color: "#ea6962",
+            },
+            {
+              name: "hackerrank",
+              url: "https://www.hackerrank.com/dashboard",
+              icon: "code-asterix",
+              icon_color: "#a9b665",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "myself",
+      background_url: "src/img/banners/cbg-9.gif",
+      categories: [
+        {
+          name: "почта",
+          links: [
+            {
+              name: "gmail",
+              url: "https://mail.google.com/mail/u/0/",
+              icon: "brand-gmail",
+              icon_color: "#ea6962",
+            },
+            {
+              name: "proton",
+              url: "https://mail.proton.me/",
+              icon: "mail",
+              icon_color: "#6F00FF"
+            }
+          ],
+        },
+        {
+          name: "хранилища",
+          links: [
+            {
+              name: "nextcloud",
+              url: "https://oto.lv.tab.digital/apps/files/",
+              icon: "brand-nextcloud",
+              icon_color: "#00BCFF",
+            },
+            {
+              name: "фото",
+              url: "https://oto.lv.tab.digital/apps/photos",
+              icon: "photo-filled",
+              icon_color: "#00bcff",
+            },
+          ],
+        },
+        {
+          name: "разное",
+          links: [
+            {
+              name: "solo",
+              url: "https://solo.to/casanovaltd",
+              icon: "circle-dashed-number-1",
+              icon_color: "#002BFF",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const CONFIG = new Config(saved_config ?? default_config);
+// const CONFIG = new Config(default_config);
+
+(function() {
+  var css = document.createElement('link');
+  css.href = 'src/css/tabler-icons.min.css';
+  css.rel = 'stylesheet';
+  css.type = 'text/css';
+  if (!CONFIG.config.localIcons)
+    document.getElementsByTagName('head')[0].appendChild(css);
+})();
