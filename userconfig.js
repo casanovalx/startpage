8718e5c7d79e09f1fe15d4dcdd1cd4e0cc38a39f
APOLLO Community Main Dev (casanovaltd) <123438051+casanovalx@users.noreply.github.com>
2025-12-02 22:59:30 +0300

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -12,8 +12,7 @@ const default_config = {
   },
   search: {
     engines: {
-      g: ["https://google.com/search?q=", "Google"],
-      d: ["https://duckduckgo.com/html?q=", "DuckDuckGo"],
+      d: ["https://duckduckgo.com/?q=", "DuckDuckGo"],
       y: ["https://youtube.com/results?search_query=", "Youtube"],
       r: ["https://www.reddit.com/search/?q=", "Reddit"],
       p: ["https://www.pinterest.com/search/pins/?q=", "Pinterest"],
